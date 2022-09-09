cf75dce36786f93985b4ca053d2256144582d7ae
edera <edera@amdocs.com>
2022-09-09 01:45:35 -0500

    Adding books view and Thymeleaf

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -25,38 +25,43 @@ public class BootstrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        System.out.println("Started in Bootstrap");
+
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("SFG Publishing");
+        publisher.setCity("St Petersburg");
+        publisher.setState("FL");
+
+        publisherRepository.save(publisher);
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
+
         Author eric = new Author("Eric", "Evans");
-        Book ddd= new Book("Domain Driven Design", "123123");
+        Book ddd = new Book("Domain Driven Design", "123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
+
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(publisher);
 
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "1212121");
-
+        Book noEJB = new Book("J2EE Development without EJB", "3939459459");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
-
-
-
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
 
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
 
-
-        System.out.println("Started in Boostrap");
-        System.out.println("Number of books: " + bookRepository.count());
-
-        Publisher penguin = new Publisher("Penguin Rn House", "Address One Line 1", "Richardson", "Texas", "18365" );
-        publisherRepository.save(penguin);
-        System.out.println("Publisher Count: " + publisherRepository.count());
-
-        ddd.setPublisher(penguin);
-        penguin.getBooks().add(ddd);
-        publisherRepository.save(penguin);
-        System.out.println("Publisher number of books: " + penguin.getBooks().size());
+        System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Publisher Number of Books: " + publisher.getBooks().size());
     }
 }
